a8dfedeeda679ce4154486703061f4697ee501c4
Martin Minkov <minkovlmartin@gmail.com>
2022-09-09 14:19:09 -0700

    Add mobile breakpoints in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.tsx"],
   theme: {
-    extend: {},
+    sm: "576px",
+    // => @media (min-width: 576px) { ... }
+
+    md: "960px",
+    // => @media (min-width: 960px) { ... }
+
+    lg: "1440px",
+    // => @media (min-width: 1440px) { ... }
   },
   variants: {
     extend: {},
