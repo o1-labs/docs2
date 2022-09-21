52ca5ecd0440fc84b26750a14d5be886e00b81e2
iregina <reginawong14@gmail.com>
2022-09-21 16:43:31 -0500

    add plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,34 @@ const katex = require("rehype-katex");
       locales: ["en", "fr", "ru"],
     },
 
-    plugins: ["docusaurus-plugin-sass"],
+    plugins: [
+      "docusaurus-plugin-sass", 
+      ["@docusaurus/plugin-client-redirects", { redirects: [
+          // /docs/oldDoc -> /docs/newDoc
+          {
+            to: '/node-operators/scan-state',
+            from: '/architecture/scan-state',
+          },
+          // // Redirect from multiple old paths to the new path
+          // {
+          //   to: '/docs/newDoc2',
+          //   from: ['/docs/oldDocFrom2019', '/docs/legacyDocFrom2016'],
+          // },
+          ],
+        // createRedirects(existingPath) {
+        //   if (existingPath.includes('/community')) {
+        //     // Redirect from /docs/team/X to /community/X and /docs/support/X to /community/X
+        //     return [
+        //       existingPath.replace('/community', '/docs/team'),
+        //       existingPath.replace('/community', '/docs/support'),
+        //     ];
+        //   }
+        //   return undefined; // Return a falsy value: no redirect created
+        // },
+        },
+      ],
+    ],
+
 
     presets: [
       [
