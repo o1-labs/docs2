1ac56e8b6c967f133d381c485c734d8d7aa802d3
Regina <iregina@users.noreply.github.com>
2022-11-03 13:00:25 -0500

    Advanced SnarkyJS Section (#96)
    * create advanced snarkyjs folder
    
    * break down pages in advancedsnarkyjs
    
    * remove the previous advanced snarkyjs page
    
    * fix broken links
    
    * fix name of sidebar to actions reducer
    
    * update url and page title for actions and reducer page

--- sidebars.js
+++ sidebars.js
@@ -36,8 +36,18 @@ module.exports = {
         'zkapps/how-to-test-a-zkapp',
         'zkapps/how-to-deploy-a-zkapp',
         'zkapps/how-to-write-a-zkapp-ui',
-        'zkapps/recursion',
-        'zkapps/advanced-snarkyjs',
+        {
+          type: 'category',
+          label: 'Advanced SnarkyJS',
+          items: [
+            "zkapps/advanced-snarkyjs/on-chain-values",
+            "zkapps/advanced-snarkyjs/events",
+            "zkapps/advanced-snarkyjs/recursion",
+            "zkapps/advanced-snarkyjs/custom-tokens",
+            "zkapps/advanced-snarkyjs/actions-and-reducer",
+            "zkapps/advanced-snarkyjs/merkle-tree",
+          ],
+        },
         {
           type: 'category',
           label: 'Tutorials',
