6fa8e6b3976f169813141e71563a879d5482976b
ymekuria <ymekuria@gmail.com>
2022-12-11 23:15:08 -0800

    feat: add custom editbutton to existing navbar item type mappings

---
+++ src/theme/NavbarItem/ComponentTypes.js
@@ -0,0 +1,7 @@
+import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes';
+import EditButton from '@site/src/components/NavbarItems/EditButton';
+
+export default {
+  ...ComponentTypes,
+  'custom-EditButton': EditButton,
+};
