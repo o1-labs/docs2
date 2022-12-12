c2f80c455c645a5d3d0cb1defe57fb18f37845a7
ymekuria <ymekuria@gmail.com>
2022-12-12 10:35:41 -0800

    feat: add mina orange button and styles to edit component

--- src/components/NavbarItems/EditButton.tsx
+++ src/components/NavbarItems/EditButton.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import Button from '@site/src/components/common/Button';
+import styles from './EditButton.module.scss';
+import ArrowRightSmall from '@site/static/svg/common/arrow_right_small.svg';
 
 export default function EditButton(): JSX.Element {
-  return <div>Edit this page</div>;
+  return (
+    <div style={{ color: 'white' }} className={styles.editButton}>
+      <Button color="orange">
+        Edit this page
+        <ArrowRightSmall />
+      </Button>
+    </div>
+  );
 }
