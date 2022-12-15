8552ea89edb34eaf9cdf5325d300a11e5755b143
ymekuria <ymekuria@gmail.com>
2022-12-15 16:05:58 -0700

    feat: add edit button above right toc sidebar

--- src/theme/DocItem/TOC/Desktop/index.js
+++ src/theme/DocItem/TOC/Desktop/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import {ThemeClassNames} from '@docusaurus/theme-common';
-import {useDoc} from '@docusaurus/theme-common/internal';
+import { ThemeClassNames } from '@docusaurus/theme-common';
+import { useDoc } from '@docusaurus/theme-common/internal';
 import TOC from '@theme/TOC';
+import Button from '@site/src/components/common/Button';
+
 export default function DocItemTOCDesktop() {
-  const {toc, frontMatter} = useDoc();
+  const { toc, frontMatter } = useDoc();
   return (
-    <TOC
-      toc={toc}
-      minHeadingLevel={frontMatter.toc_min_heading_level}
-      maxHeadingLevel={frontMatter.toc_max_heading_level}
-      className={ThemeClassNames.docs.docTocDesktop}
-    />
+    <>
+      <Button color="orange">Edit this page</Button>
+      <TOC
+        toc={toc}
+        minHeadingLevel={frontMatter.toc_min_heading_level}
+        maxHeadingLevel={frontMatter.toc_max_heading_level}
+        className={ThemeClassNames.docs.docTocDesktop}
+      />
+    </>
   );
 }
