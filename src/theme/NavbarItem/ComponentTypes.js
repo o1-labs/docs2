62ca6e0d433c8dd8bb2d19582dabe5e14e0ac3f7
ymekuria <ymekuria@gmail.com>
2022-12-16 01:22:27 -0700

    refactor: remove navbar items custom compoent type mappings

--- src/theme/NavbarItem/ComponentTypes.js
+++
@@ -1,7 +0,0 @@
-import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes';
-import EditButton from '@site/src/components/NavbarItems/EditButton';
-
-export default {
-  ...ComponentTypes,
-  'custom-EditButton': EditButton,
-};
