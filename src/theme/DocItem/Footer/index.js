0b2f016242442060197837d54075fdc47d6f0e21
ymekuria <ymekuria@gmail.com>
2022-12-20 02:00:59 -0700

    feat: remove edit page link from footer

--- src/theme/DocItem/Footer/index.js
+++ src/theme/DocItem/Footer/index.js
@@ -3,7 +3,6 @@ import clsx from 'clsx';
 import { ThemeClassNames } from '@docusaurus/theme-common';
 import { useDoc } from '@docusaurus/theme-common/internal';
 import LastUpdated from '@theme/LastUpdated';
-import EditThisPage from '@theme/EditThisPage';
 import TagsListInline from '@theme/TagsListInline';
 import styles from './DocItemFooter.module.scss';
 function TagsRow(props) {
@@ -28,7 +27,6 @@ function EditMetaRow({
 }) {
   return (
     <div className={clsx(ThemeClassNames.docs.docFooterEditMetaRow, 'row')}>
-      <div className="col">{editUrl && <EditThisPage editUrl={editUrl} />}</div>
       <div className={clsx('col', styles.lastUpdated)}>
         {(lastUpdatedAt || lastUpdatedBy) && (
           <LastUpdated
