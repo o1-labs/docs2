dcdba0f2e6ec94779690a82d4710a9ec196ab2c2
Nic Young <nic@wefarm.com>
2022-12-22 16:27:23 +0000

    Updated examples 1 - 4 to align with snarkyjs 0.8.0.
    Rewrote tutorial 1 to align with updates to example 1.
    
    Updated links to example code, which is now located within this repo.
    
    Still outstanding:
    - examples 5 - 8
    - tutorials 2 - 8

--- docs/zkapps/examples/01-hello-world/src/main.ts
+++ docs/zkapps/examples/01-hello-world/src/main.ts
@@ -16,12 +16,12 @@ const useProof = false;
 
 const Local = Mina.LocalBlockchain({ proofsEnabled: useProof });
 Mina.setActiveInstance(Local);
-const { privateKey: deployerKey, publicKey: deployerAccount } =
-  Local.testAccounts[0];
+const { privateKey: deployerKey, publicKey: deployerAccount } = Local.testAccounts[0];
+const { privateKey: senderKey, publicKey: senderAccount } = Local.testAccounts[1];
 
 // ----------------------------------------------------
 
-// create a destination we will deploy the smart contract to
+// Create a public/private key pair. The public key is our address and where we will deploy to
 const zkAppPrivateKey = PrivateKey.random();
 const zkAppAddress = zkAppPrivateKey.toPublicKey();
 
@@ -39,11 +39,11 @@ console.log('state after init:', num0.toString());
 
 // ----------------------------------------------------
 
-const txn1 = await Mina.transaction(deployerKey, () => {
+const txn1 = await Mina.transaction(senderAccount, () => {
   zkAppInstance.update(Field(9));
 });
 await txn1.prove();
-await txn1.send();
+await txn1.sign([senderKey]).send();
 
 const num1 = zkAppInstance.num.get();
 console.log('state after txn1:', num1.toString());
@@ -51,11 +51,11 @@ console.log('state after txn1:', num1.toString());
 // ----------------------------------------------------
 
 try {
-  const txn2 = await Mina.transaction(deployerKey, () => {
+  const txn2 = await Mina.transaction(senderAccount, () => {
     zkAppInstance.update(Field(75));
   });
   await txn1.prove();
-  await txn2.send();
+  await txn2.sign([senderKey]).send();
 } catch (ex: any) {
   console.log(ex.message);
 }
@@ -64,16 +64,11 @@ console.log('state after txn2:', num2.toString());
 
 // ----------------------------------------------------
 
-const txn3 = await Mina.transaction(deployerKey, () => {
+const txn3 = await Mina.transaction(senderAccount, () => {
   zkAppInstance.update(Field(81));
-  if (!useProof) {
-    zkAppInstance.sign(zkAppPrivateKey);
-  }
 });
-if (useProof) {
-  await txn3.prove();
-}
-await txn3.send();
+await txn3.prove();
+await txn3.sign([senderKey]).send();
 
 const num3 = zkAppInstance.num.get();
 console.log('state after txn3:', num3.toString());
