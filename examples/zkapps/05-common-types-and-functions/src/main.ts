8299dedce2683d364d610eb5dd382e87e8147b41
Gregor <gregor.mitscha-baude@gmx.at>
2023-01-13 15:19:42 +0100

    update example 5

--- examples/zkapps/05-common-types-and-functions/src/main.ts
+++ examples/zkapps/05-common-types-and-functions/src/main.ts
@@ -1,9 +1,7 @@
-
 import {
   Mina,
   isReady,
   shutdown,
-  Bool,
   UInt32,
   UInt64,
   Int64,
@@ -13,29 +11,29 @@ import {
   Signature,
   Poseidon,
   Field,
-  CircuitValue,
-  prop,
-  arrayProp,
   Circuit,
   MerkleWitness,
   MerkleTree,
-  AccountUpdate
-} from 'snarkyjs'
+  AccountUpdate,
+  Struct,
+  MerkleMap,
+  Bool,
+} from 'snarkyjs';
 
-import { LedgerContract } from './LedgerContract.js'
-import { BasicMerkleTreeContract } from './BasicMerkleTreeContract.js'
+import { LedgerContract } from './LedgerContract.js';
+import { BasicMerkleTreeContract } from './BasicMerkleTreeContract.js';
 
 await isReady;
 
 // --------------------------------------
+console.log('--------------------------------------');
 
 const num1 = UInt32.from(40);
 const num2 = UInt64.from(40);
 
-const num1EqualsNum2: string = num1.toUInt64().equals(num2).toString();
+const num1EqualsNum2: Bool = num1.toUInt64().equals(num2);
 
-console.log("// --------------------------------------");
-console.log(`num1 == num2: ${num1EqualsNum2}`);
+console.log(`num1 == num2: ${num1EqualsNum2.toString()}`);
 console.log(`Fields in num1: ${num1.toFields().length}`);
 
 // --------------------------------------
@@ -52,12 +50,12 @@ console.log(`Fields in signedNum1: ${signedNum1.toFields().length}`);
 
 const char1 = Character.fromString('c');
 const char2 = Character.fromString('d');
-const char1EqualsChar2: string = char1.equals(char2).toString();
+const char1EqualsChar2: Bool = char1.equals(char2);
 
 console.log(`char1: ${char1}`);
-console.log(`char1 == char2: ${char1EqualsChar2}`);
+console.log(`char1 == char2: ${char1EqualsChar2.toString()}`);
 console.log(`Fields in char1: ${char1.toFields().length}`);
-console.log("// --------------------------------------");
+console.log('--------------------------------------');
 
 // --------------------------------------
 
@@ -70,7 +68,7 @@ console.log(`Fields in str1: ${str1.toFields().length}`);
 const zkAppPrivateKey = PrivateKey.random();
 const zkAppPublicKey = zkAppPrivateKey.toPublicKey();
 
-const data1 = char2.toFields().concat(signedNumSum.toFields())
+const data1 = char2.toFields().concat(signedNumSum.toFields());
 const data2 = char1.toFields().concat(str1.toFields());
 
 const signature = Signature.create(zkAppPrivateKey, data2);
@@ -87,44 +85,34 @@ console.log(`signature verified for data1: ${verifiedData1}`);
 console.log(`signature verified for data2: ${verifiedData2}`);
 
 console.log(`Fields in signature: ${signature.toFields().length}`);
-console.log("// --------------------------------------");
+console.log('--------------------------------------');
 
 // --------------------------------------
 
-class Point extends CircuitValue {
-  @prop x: Field;
-  @prop y: Field;
-
-  static fromField(x: Field, y: Field) {
-    return new Point(x, y);
-  }
-
-  static addPoints(a: Point, b: Point) {
-    return new Point(a.x.add(b.x), a.y.add(b.y));
+class Point extends Struct({ x: Field, y: Field }) {
+  static add(a: Point, b: Point) {
+    return { x: a.x.add(b.x), y: a.y.add(b.y) };
   }
 }
 
-const point1 = Point.fromField(Field(10), Field(4));
-const point2 = Point.fromField(Field(1), Field(2));
+const point1 = { x: Field(10), y: Field(4) };
+const point2 = { x: Field(1), y: Field(2) };
 
-const pointSum = Point.addPoints(point1, point2);
+const pointSum = Point.add(point1, point2);
 
-console.log(`pointSum Fields: ${pointSum.toFields()}`);
+console.log(`pointSum Fields: ${Point.toFields(pointSum)}`);
 
-class Points8 extends CircuitValue {
-  @arrayProp(Point, 8) points: Point[];
+class Points8 extends Struct({
+  points: [Point, Point, Point, Point, Point, Point, Point, Point],
+}) {}
 
-  constructor(points: Point[]) {
-    super();
-    this.points = points;
-  }
-}
+const points = new Array(8)
+  .fill(null)
+  .map((_, i) => ({ x: Field(i), y: Field(i * 10) }));
+const points8: Points8 = { points };
 
-const pointsArray = new Array(8).fill(null).map((_, i) => Point.fromField(Field(i), Field(i*10)));
-const points8 = new Points8(pointsArray);
-
-console.log(`points8 Fields: ${JSON.stringify(points8)}`);
-console.log("// --------------------------------------");
+console.log(`points8 JSON: ${JSON.stringify(points8)}`);
+console.log('--------------------------------------');
 
 // --------------------------------------
 
@@ -133,73 +121,117 @@ const input2 = Int64.from(-15);
 
 const inputSum = input1.add(input2);
 
-const inputSumAbs = Circuit.if(inputSum.isPositive(), inputSum, inputSum.mul(Int64.minusOne));
+const inputSumAbs = Circuit.if(
+  inputSum.isPositive(),
+  inputSum,
+  inputSum.mul(Int64.minusOne)
+);
 
 console.log(`inputSum: ${inputSum}`);
 console.log(`inputSumAbs: ${inputSumAbs}`);
 
 const input3 = Int64.from(22);
 
-const input1largest = input1.sub(input2).isPositive().and(input1.sub(input3).isPositive());
-const input2largest = input2.sub(input1).isPositive().and(input2.sub(input3).isPositive());
-const input3largest = input3.sub(input1).isPositive().and(input3.sub(input2).isPositive());
+const input1largest = input1
+  .sub(input2)
+  .isPositive()
+  .and(input1.sub(input3).isPositive());
+const input2largest = input2
+  .sub(input1)
+  .isPositive()
+  .and(input2.sub(input3).isPositive());
+const input3largest = input3
+  .sub(input1)
+  .isPositive()
+  .and(input3.sub(input2).isPositive());
 
-const largest = Circuit.switch([ input1largest, input2largest, input3largest ], Int64, [ input1, input2, input3 ])
+const largest = Circuit.switch(
+  [input1largest, input2largest, input3largest],
+  Int64,
+  [input1, input2, input3]
+);
 
 console.log(`largest: ${largest}`);
-console.log("// --------------------------------------");
+console.log('--------------------------------------');
 
 // --------------------------------------
 
 const Local = Mina.LocalBlockchain();
 Mina.setActiveInstance(Local);
-const { privateKey: deployerKey, publicKey: deployerAccount } = Local.testAccounts[0];
-const { privateKey: senderPrivateKey, publicKey: senderPublicKey } = Local.testAccounts[1];
+const { privateKey: deployerKey, publicKey: deployerAccount } =
+  Local.testAccounts[0];
+const { privateKey: senderPrivateKey, publicKey: senderPublicKey } =
+  Local.testAccounts[1];
 
 // --------------------------------------
-// create a new merkle tree and BasicMerkleTreeContract zkapp account 
+// create a new merkle tree and BasicMerkleTreeContract zkapp account
 
 {
-
   const basicTreeZkAppPrivateKey = PrivateKey.random();
   const basicTreeZkAppAddress = basicTreeZkAppPrivateKey.toPublicKey();
 
+  // initialize the zkapp
+  const zkApp = new BasicMerkleTreeContract(basicTreeZkAppAddress);
+  await BasicMerkleTreeContract.compile();
+
+  // create a new tree
   const height = 20;
   const tree = new MerkleTree(height);
   class MerkleWitness20 extends MerkleWitness(height) {}
 
-  const zkApp = new BasicMerkleTreeContract(basicTreeZkAppAddress);
-  await BasicMerkleTreeContract.compile()
-
+  // deploy the smart contract
   const deployTxn = await Mina.transaction(deployerAccount, () => {
     AccountUpdate.fundNewAccount(deployerAccount);
     zkApp.deploy();
+    // get the root of the new tree to use as the initial tree root
     zkApp.initState(tree.getRoot());
   });
   await deployTxn.prove();
-  await deployTxn.sign([deployerKey, basicTreeZkAppPrivateKey]).send();
+  deployTxn.sign([deployerKey, basicTreeZkAppPrivateKey]);
 
-  const incrementIndex = 522;
+  const pendingDeployTx = await deployTxn.send();
+  /**
+   * `txn.send()` returns a pending transaction with two methods - `.wait()` and `.hash()`
+   * `.hash()` returns the transaction hash
+   * `.wait()` automatically resolves once the transaction has been included in a block. this is redundant for the LocalBlockchain, but very helpful for live testnets
+   */
+  await pendingDeployTx.wait();
+
+  const incrementIndex = 522n;
   const incrementAmount = Field(9);
 
-  const witness = new MerkleWitness20(tree.getWitness(BigInt(incrementIndex)));
-  tree.setLeaf(BigInt(incrementIndex), incrementAmount);
+  // get the witness for the current tree
+  const witness = new MerkleWitness20(tree.getWitness(incrementIndex));
 
+  // update the leaf locally
+  tree.setLeaf(incrementIndex, incrementAmount);
+
+  // update the smart contract
   const txn1 = await Mina.transaction(senderPublicKey, () => {
-    zkApp.update(witness, Field(0), incrementAmount);
+    zkApp.update(
+      witness,
+      Field(0), // leafs in new trees start at a state of 0
+      incrementAmount
+    );
   });
   await txn1.prove();
-  await txn1.sign([senderPrivateKey, zkAppPrivateKey]).send();
-  
-  console.log(`BasicMerkleTree: local tree root hash after send1: ${tree.getRoot()}`);
-  console.log(`BasicMerkleTree: smart contract root hash after send1: ${zkApp.treeRoot.get()}`);
+  const pendingTx = await txn1.sign([senderPrivateKey, zkAppPrivateKey]).send();
+  await pendingTx.wait();
+
+  // compare the root of the smart contract tree to our local tree
+  console.log(
+    `BasicMerkleTree: local tree root hash after send1: ${tree.getRoot()}`
+  );
+  console.log(
+    `BasicMerkleTree: smart contract root hash after send1: ${zkApp.treeRoot.get()}`
+  );
 }
 
+console.log('--------------------------------------');
 // --------------------------------------
-// create a new merkle tree and LedgerContract zkapp account 
+// create a new merkle tree and LedgerContract zkapp account
 
 {
-
   const ledgerZkAppPrivateKey = PrivateKey.random();
   const ledgerZkAppAddress = ledgerZkAppPrivateKey.toPublicKey();
 
@@ -213,16 +245,22 @@ const { privateKey: senderPrivateKey, publicKey: senderPublicKey } = Local.testA
   const recipientPrivateKey = PrivateKey.random();
   const recipientPublicKey = recipientPrivateKey.toPublicKey();
 
-  const senderAccount = 10;
-  const recipientAccount = 500;
+  const senderAccount = 10n;
+  const recipientAccount = 500n;
 
   tree.setLeaf(
-    BigInt(senderAccount), 
-    Poseidon.hash([ senderInitialBalance, Poseidon.hash(senderPublicKey.toFields()) ])
+    senderAccount,
+    Poseidon.hash([
+      senderInitialBalance,
+      Poseidon.hash(senderPublicKey.toFields()),
+    ])
   );
   tree.setLeaf(
-    BigInt(recipientAccount), 
-    Poseidon.hash([ recipientInitialBalance, Poseidon.hash(recipientPublicKey.toFields()) ])
+    recipientAccount,
+    Poseidon.hash([
+      recipientInitialBalance,
+      Poseidon.hash(recipientPublicKey.toFields()),
+    ])
   );
 
   const zkApp = new LedgerContract(ledgerZkAppAddress);
@@ -233,7 +271,7 @@ const { privateKey: senderPrivateKey, publicKey: senderPublicKey } = Local.testA
     zkApp.deploy();
     zkApp.initState(tree.getRoot());
   });
-  await deployTxn.prove()
+  await deployTxn.prove();
   await deployTxn.sign([deployerKey, ledgerZkAppPrivateKey]).send();
 
   // --------------------------------------
@@ -241,79 +279,116 @@ const { privateKey: senderPrivateKey, publicKey: senderPublicKey } = Local.testA
 
   const amount = Field(12);
 
-  const newSenderBalance = senderInitialBalance.sub(amount)
+  const newSenderBalance = senderInitialBalance.sub(amount);
 
-  const sendWitness1 = new MerkleWitness20(tree.getWitness(BigInt(senderAccount)));
+  const sendWitness1 = new MerkleWitness20(tree.getWitness(senderAccount));
   tree.setLeaf(
-    BigInt(senderAccount), 
-    Poseidon.hash([ newSenderBalance, Poseidon.hash(senderPublicKey.toFields()) ])
+    senderAccount,
+    Poseidon.hash([newSenderBalance, Poseidon.hash(senderPublicKey.toFields())])
   );
-  const recipientWitness1 = new MerkleWitness20(tree.getWitness(BigInt(recipientAccount)));
+  const recipientWitness1 = new MerkleWitness20(
+    tree.getWitness(recipientAccount)
+  );
 
   tree.setLeaf(
-    BigInt(recipientAccount), 
-    Poseidon.hash([ recipientInitialBalance.add(amount), Poseidon.hash(recipientPublicKey.toFields()) ])
+    recipientAccount,
+    Poseidon.hash([
+      recipientInitialBalance.add(amount),
+      Poseidon.hash(recipientPublicKey.toFields()),
+    ])
   );
 
   const signature1 = Signature.create(
-    senderPrivateKey, 
-    [ zkApp.ledgerRoot.get(), amount ].concat(recipientPublicKey.toFields())
+    senderPrivateKey,
+    [zkApp.ledgerRoot.get(), amount].concat(recipientPublicKey.toFields())
   );
 
   const txn1 = await Mina.transaction(senderPublicKey, () => {
     zkApp.sendBalance(
-      sendWitness1, 
-      recipientWitness1, 
-      senderInitialBalance, 
-      recipientInitialBalance, 
+      sendWitness1,
+      recipientWitness1,
+      senderInitialBalance,
+      recipientInitialBalance,
       senderPublicKey,
       recipientPublicKey,
       signature1,
-      amount);
+      amount
+    );
   });
 
   await txn1.prove();
 
   await txn1.sign([senderPrivateKey, ledgerZkAppPrivateKey]).send();
 
-  console.log(`LedgerContract: local tree root hash after send1: ${tree.getRoot()}`);
-  console.log(`LedgerContract: smart contract root hash after send1: ${zkApp.ledgerRoot.get()}`);
+  console.log(
+    `LedgerContract: local tree root hash after send1: ${tree.getRoot()}`
+  );
+  console.log(
+    `LedgerContract: smart contract root hash after send1: ${zkApp.ledgerRoot.get()}`
+  );
 
   // --------------------------------------
   // send from the sender to a recipient that wasn't in the account before
 
   const newRecipientPublicKey = PrivateKey.random().toPublicKey();
-  const newRecipientAccount = 10000;
+  const newRecipientAccount = 10000n;
 
-  const sendWitness2 = new MerkleWitness20(tree.getWitness(BigInt(senderAccount)));
-  tree.setLeaf(BigInt(senderAccount), Poseidon.hash([ newSenderBalance.sub(amount), Poseidon.hash(senderPublicKey.toFields()) ]));
-  const recipientWitness2 = new MerkleWitness20(tree.getWitness(BigInt(newRecipientAccount)));
+  const sendWitness2 = new MerkleWitness20(tree.getWitness(senderAccount));
+  tree.setLeaf(
+    senderAccount,
+    Poseidon.hash([
+      newSenderBalance.sub(amount),
+      Poseidon.hash(senderPublicKey.toFields()),
+    ])
+  );
+  const recipientWitness2 = new MerkleWitness20(
+    tree.getWitness(newRecipientAccount)
+  );
 
-  tree.setLeaf(BigInt(newRecipientAccount), Poseidon.hash([ amount, Poseidon.hash(newRecipientPublicKey.toFields()) ]));
+  tree.setLeaf(
+    newRecipientAccount,
+    Poseidon.hash([amount, Poseidon.hash(newRecipientPublicKey.toFields())])
+  );
 
   const signature2 = Signature.create(
-    senderPrivateKey, 
-    [ zkApp.ledgerRoot.get(), amount ].concat(newRecipientPublicKey.toFields())
+    senderPrivateKey,
+    [zkApp.ledgerRoot.get(), amount].concat(newRecipientPublicKey.toFields())
   );
 
   const txn2 = await Mina.transaction(senderPublicKey, () => {
     zkApp.sendBalance(
-      sendWitness2, 
-      recipientWitness2, 
-      newSenderBalance, 
-      Field(0), 
+      sendWitness2,
+      recipientWitness2,
+      newSenderBalance,
+      Field(0),
       senderPublicKey,
       newRecipientPublicKey,
       signature2,
-      amount);
+      amount
+    );
   });
   await txn2.prove();
   await txn2.sign([senderPrivateKey, ledgerZkAppPrivateKey]).send();
 
-  console.log(`LedgerContract: local tree root hash after send2: ${tree.getRoot()}`);
-  console.log(`LedgerContract: smart contract root hash after send2: ${zkApp.ledgerRoot.get()}`);
-
+  console.log(
+    `LedgerContract: local tree root hash after send2: ${tree.getRoot()}`
+  );
+  console.log(
+    `LedgerContract: smart contract root hash after send2: ${zkApp.ledgerRoot.get()}`
+  );
 }
+
+// --------------------------------------
+console.log('--------------------------------------');
+
+const map = new MerkleMap();
+
+const key = Field(100);
+const value = Field(50);
+
+map.set(key, value);
+
+console.log(`value for key ${key}: ${map.get(key)}`);
 
 // --------------------------------------
 
