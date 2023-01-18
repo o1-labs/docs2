28a76213f3e640a21a05f3dc3c484ac8fdc144ae
ymekuria <ymekuria@gmail.com>
2023-01-18 00:22:59 -0700

    refactor: update contract tests to be consistent with cli template

--- examples/zkapps/04-zkapp-browser-ui/contracts/src/Add.test.ts
+++ examples/zkapps/04-zkapp-browser-ui/contracts/src/Add.test.ts
@@ -1,12 +1,12 @@
 import { Add } from './Add';
 import {
-  AccountUpdate,
-  Field,
   isReady,
+  shutdown,
+  Field,
   Mina,
   PrivateKey,
   PublicKey,
-  shutdown
+  AccountUpdate,
 } from 'snarkyjs';
 
 /*
@@ -16,26 +16,7 @@ import {
  * See https://docs.minaprotocol.com/zkapps for more info.
  */
 
-const proofsEnabled = false;
-
-function createLocalBlockchain() {
-  const Local = Mina.LocalBlockchain({proofsEnabled});
-  Mina.setActiveInstance(Local);
-  return Local.testAccounts;
-}
-
-async function localDeploy(
-  zkAppInstance: Add,
-  zkAppPrivateKey: PrivateKey,
-  deployerAccount: PublicKey,
-  deployerKey: PrivateKey
-) {
-  const txn = await Mina.transaction(deployerAccount, () => {
-    AccountUpdate.fundNewAccount(deployerAccount);
-    zkAppInstance.deploy();
-  });
-  await txn.sign([deployerKey, zkAppPrivateKey]).send();
-}
+let proofsEnabled = false;
 
 describe('Add', () => {
   let deployerAccount: PublicKey,
@@ -44,41 +25,59 @@ describe('Add', () => {
     senderKey: PrivateKey,
     zkAppAddress: PublicKey,
     zkAppPrivateKey: PrivateKey,
-    testAccounts: { publicKey: PublicKey; privateKey: PrivateKey; }[];
+    zkApp: Add;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     await isReady;
-    testAccounts = createLocalBlockchain(); 
-    ({ privateKey: deployerKey, publicKey: deployerAccount } = testAccounts[0]);
-    ({ privateKey: senderKey, publicKey: senderAccount } = testAccounts[1]);
-    zkAppPrivateKey = PrivateKey.random();
-    zkAppAddress = zkAppPrivateKey.toPublicKey();
+    if (proofsEnabled) Add.compile();
   });
 
-  afterAll(async () => {
+  beforeEach(() => {
+    const Local = Mina.LocalBlockchain({ proofsEnabled });
+    Mina.setActiveInstance(Local);
+    ({ privateKey: deployerKey, publicKey: deployerAccount } =
+      Local.testAccounts[0]);
+    ({ privateKey: senderKey, publicKey: senderAccount } =
+      Local.testAccounts[1]);
+    zkAppPrivateKey = PrivateKey.random();
+    zkAppAddress = zkAppPrivateKey.toPublicKey();
+    zkApp = new Add(zkAppAddress);
+  });
+
+  afterAll(() => {
     // `shutdown()` internally calls `process.exit()` which will exit the running Jest process early.
     // Specifying a timeout of 0 is a workaround to defer `shutdown()` until Jest is done running all tests.
     // This should be fixed with https://github.com/MinaProtocol/mina/issues/10943
     setTimeout(shutdown, 0);
   });
 
+  async function localDeploy() {
+    const txn = await Mina.transaction(deployerAccount, () => {
+      AccountUpdate.fundNewAccount(deployerAccount);
+      zkApp.deploy();
+    });
+    await txn.prove();
+    // this tx needs .sign(), because `deploy()` adds an account update that requires signature authorization
+    await txn.sign([deployerKey, zkAppPrivateKey]).send();
+  }
+
   it('generates and deploys the `Add` smart contract', async () => {
-    const zkAppInstance = new Add(zkAppAddress);
-    await localDeploy(zkAppInstance, zkAppPrivateKey, deployerAccount, deployerKey);
-    const num = zkAppInstance.num.get();
+    await localDeploy();
+    const num = zkApp.num.get();
     expect(num).toEqual(Field(1));
   });
 
   it('correctly updates the num state on the `Add` smart contract', async () => {
-    const zkAppInstance = new Add(zkAppAddress);
-    await localDeploy(zkAppInstance, zkAppPrivateKey, deployerAccount, deployerKey);
+    await localDeploy();
+
+    // update transaction
     const txn = await Mina.transaction(senderAccount, () => {
-      zkAppInstance.update();
+      zkApp.update();
     });
     await txn.prove();
     await txn.sign([senderKey]).send();
 
-    const updatedNum = zkAppInstance.num.get();
+    const updatedNum = zkApp.num.get();
     expect(updatedNum).toEqual(Field(3));
   });
 });
