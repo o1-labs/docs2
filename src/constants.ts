3bc77f9b25a5dd2b04fdf89b923ccd0982520576
Martin Minkov <minkovlmartin@gmail.com>
2023-04-24 15:44:26 -0700

    feat: Update form submission

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,12 @@
 export const SocialLinks = {
-  Github: "https://github.com/MinaProtocol/mina",
-  Twitter: "https://twitter.com/minaprotocol",
-  WeChat: "https://forums.minaprotocol.com/",
-  Youtube: "https://www.youtube.com/channel/UCWzKMFfIlMzHUXKSnYot5HA",
-  Facebook: "http://bit.ly/MinaProtocolFacebook",
-  Discord: "https://bit.ly/MinaDiscord",
-  Telegram: "http://bit.ly/MinaTelegram",
-  Support: "https://support.minaprotocol.com",
+  Github: 'https://github.com/MinaProtocol/mina',
+  Twitter: 'https://twitter.com/minaprotocol',
+  WeChat: 'https://forums.minaprotocol.com/',
+  Youtube: 'https://www.youtube.com/channel/UCWzKMFfIlMzHUXKSnYot5HA',
+  Facebook: 'http://bit.ly/MinaProtocolFacebook',
+  Discord: 'https://bit.ly/MinaDiscord',
+  Telegram: 'http://bit.ly/MinaTelegram',
+  Support: 'https://support.minaprotocol.com',
 };
+
+export const FormSubmitUrl = 'https://minaprotocol.com/docs-footer-subscribe';
