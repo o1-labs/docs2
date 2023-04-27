3c699593cba83567f9a0998a5d7c1d3bd4991ba4
barriebyron <barrie@o1labs.org>
2023-04-27 17:28:20 -0400

    failed attempt to update sidebar, need eagle eyes

--- sidebars.js
+++ sidebars.js
@@ -34,259 +34,268 @@ module.exports = {
       items: [
         'zkapps/index',
         'zkapps/how-zkapps-work',
+        {
+          type: 'category',
+          label: 'Using zkApps',
+          items: [
         'zkapps/how-to-write-a-zkapp',
+        'zkapps/how-to-write-a-zkapp-ui',
         'zkapps/how-to-test-a-zkapp',
         'zkapps/how-to-deploy-a-zkapp',
-        'zkapps/how-to-write-a-zkapp-ui',
         {
-          type: 'category',
-          label: 'Advanced SnarkyJS',
-          items: [
-            'zkapps/advanced-snarkyjs/fetch-events-and-actions',
-            'zkapps/advanced-snarkyjs/events',
-            'zkapps/advanced-snarkyjs/actions-and-reducer',
-            'zkapps/advanced-snarkyjs/on-chain-values',
-            'zkapps/advanced-snarkyjs/recursion',
-            'zkapps/advanced-snarkyjs/custom-tokens',
-            'zkapps/advanced-snarkyjs/merkle-tree',
-            'zkapps/advanced-snarkyjs/permissions',
-            'zkapps/advanced-snarkyjs/time-locked-accounts',
-          ],
-        },
-        {
-          type: 'category',
-          label: 'Tutorials',
-          items: [
-            'zkapps/tutorials/hello-world',
-            'zkapps/tutorials/private-inputs-hash-functions',
-            'zkapps/tutorials/deploying-to-a-network',
-            'zkapps/tutorials/zkapp-ui-with-react',
-            'zkapps/tutorials/common-types-and-functions',
-            'zkapps/tutorials/offchain-storage',
-            'zkapps/tutorials/oracle',
-            'zkapps/tutorials/custom-tokens',
-            'zkapps/tutorials/recursion',
-            'zkapps/tutorials/anonymous-message-board',
-            'zkapps/tutorials/interacting-with-zkapps-server-side',
-          ],
-        },
-        {
-          type: 'category',
-          label: 'SnarkyJS Reference',
-          items: [
-            {
-              type: 'doc',
-              id: 'zkapps/snarkyjs-reference/README',
-              label: 'Overview',
-            },
-            {
-              type: 'category',
-              label: 'Classes',
-              items: [
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/AccountUpdate',
-                  label: 'AccountUpdate',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/Bool',
-                  label: 'Bool',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/Character',
-                  label: 'Character',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/Circuit',
-                  label: 'Circuit',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/CircuitString',
-                  label: 'CircuitString',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/CircuitValue',
-                  label: 'CircuitValue',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/Field',
-                  label: 'Field',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/Group',
-                  label: 'Group',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/Int64',
-                  label: 'Int64',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/Keypair',
-                  label: 'Keypair',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/Ledger',
-                  label: 'Ledger',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/MerkleMap',
-                  label: 'MerkleMap',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/MerkleMapWitness',
-                  label: 'MerkleMapWitness',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/MerkleTree',
-                  label: 'MerkleTree',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/PrivateKey',
-                  label: 'PrivateKey',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/Proof',
-                  label: 'Proof',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/Scalar',
-                  label: 'Scalar',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/SelfProof',
-                  label: 'SelfProof',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/Sign',
-                  label: 'Sign',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/Signature',
-                  label: 'Signature',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/SmartContract',
-                  label: 'SmartContract',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/Token',
-                  label: 'Token',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/TokenSymbol',
-                  label: 'TokenSymbol',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/Types.PublicKey',
-                  label: 'Types.PublicKey',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/UInt32',
-                  label: 'UInt32',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/UInt64',
-                  label: 'UInt64',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/classes/VerificationKey',
-                  label: 'VerificationKey',
-                },
-              ],
-            },
-            {
-              type: 'category',
-              label: 'Interfaces',
-              items: [
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/interfaces/Mina.TransactionId',
-                  label: 'Mina.TransactionId',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/interfaces/Permissions',
-                  label: 'Permissions',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/interfaces/Provable',
-                  label: 'Provable',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/interfaces/ProvablePure',
-                  label: 'ProvablePure',
-                },
-              ],
-            },
-            {
-              type: 'category',
-              label: 'Modules',
-              items: [
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/modules/Encoding',
-                  label: 'Encoding',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/modules/Encryption',
-                  label: 'Encryption',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/modules/Experimental',
-                  label: 'Experimental',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/modules/Mina',
-                  label: 'Mina',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/modules/Types.Json',
-                  label: 'Types.Json',
-                },
-                {
-                  type: 'doc',
-                  id: 'zkapps/snarkyjs-reference/modules/Types',
-                  label: 'Types',
-                },
-              ],
-            },
-          ],
-        },
-        'zkapps/roadmap',
-        'zkapps/faq',
-        'zkapps/zkapps-for-ethereum-developers',
+        type: 'category',
+        label: 'SnarkyJS',
+        items: [
+          'zkapps/snarkyjs/snarkyjs-overview',
+          'zkapps/snarkyjs/snarkyjs-intro',
+          'zkapps/snarkyjs/writing-circuits',
+          'zkapps/snarkyjs/writing-smart-contracts',
+          'zkapps/snarkyjs/interact-with-mina',
+          'zkapps/snarkyjs/fetch-events-and-actions',
+          'zkapps/snarkyjs/events',
+          'zkapps/snarkyjs/actions-and-reducer',
+          'zkapps/snarkyjs/on-chain-values',
+          'zkapps/snarkyjs/recursion',
+          'zkapps/snarkyjs/custom-tokens',
+          'zkapps/snarkyjs/merkle-tree',
+          'zkapps/snarkyjs/permissions',
+          'zkapps/snarkyjs/time-locked-accounts',
+        ],
+      },
+      {
+        type: 'category',
+        label: 'Tutorials',
+        items: [
+          'zkapps/tutorials/hello-world',
+          'zkapps/tutorials/private-inputs-hash-functions',
+          'zkapps/tutorials/deploying-to-a-network',
+          'zkapps/tutorials/zkapp-ui-with-react',
+          'zkapps/tutorials/common-types-and-functions',
+          'zkapps/tutorials/offchain-storage',
+          'zkapps/tutorials/oracle',
+          'zkapps/tutorials/custom-tokens',
+          'zkapps/tutorials/recursion',
+          'zkapps/tutorials/anonymous-message-board',
+          'zkapps/tutorials/interacting-with-zkapps-server-side',
+        ],
+      },
+      {
+        type: 'category',
+        label: 'SnarkyJS Reference',
+        items: [
+          {
+            type: 'doc',
+            id: 'zkapps/snarkyjs-reference/README',
+            label: 'Overview',
+          },
+          {
+            type: 'category',
+            label: 'Classes',
+            items: [
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/AccountUpdate',
+                label: 'AccountUpdate',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/Bool',
+                label: 'Bool',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/Character',
+                label: 'Character',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/Circuit',
+                label: 'Circuit',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/CircuitString',
+                label: 'CircuitString',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/CircuitValue',
+                label: 'CircuitValue',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/Field',
+                label: 'Field',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/Group',
+                label: 'Group',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/Int64',
+                label: 'Int64',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/Keypair',
+                label: 'Keypair',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/Ledger',
+                label: 'Ledger',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/MerkleMap',
+                label: 'MerkleMap',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/MerkleMapWitness',
+                label: 'MerkleMapWitness',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/MerkleTree',
+                label: 'MerkleTree',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/PrivateKey',
+                label: 'PrivateKey',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/Proof',
+                label: 'Proof',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/Scalar',
+                label: 'Scalar',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/SelfProof',
+                label: 'SelfProof',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/Sign',
+                label: 'Sign',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/Signature',
+                label: 'Signature',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/SmartContract',
+                label: 'SmartContract',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/Token',
+                label: 'Token',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/TokenSymbol',
+                label: 'TokenSymbol',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/Types.PublicKey',
+                label: 'Types.PublicKey',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/UInt32',
+                label: 'UInt32',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/UInt64',
+                label: 'UInt64',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/classes/VerificationKey',
+                label: 'VerificationKey',
+              },
+            ],
+          },
+          {
+            type: 'category',
+            label: 'Interfaces',
+            items: [
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/interfaces/Mina.TransactionId',
+                label: 'Mina.TransactionId',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/interfaces/Permissions',
+                label: 'Permissions',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/interfaces/Provable',
+                label: 'Provable',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/interfaces/ProvablePure',
+                label: 'ProvablePure',
+              },
+            ],
+          },
+          {
+            type: 'category',
+            label: 'Modules',
+            items: [
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/modules/Encoding',
+                label: 'Encoding',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/modules/Encryption',
+                label: 'Encryption',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/modules/Experimental',
+                label: 'Experimental',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/modules/Mina',
+                label: 'Mina',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/modules/Types.Json',
+                label: 'Types.Json',
+              },
+              {
+                type: 'doc',
+                id: 'zkapps/snarkyjs-reference/modules/Types',
+                label: 'Types',
+              },
+            ],
+          },
+        ],
+      },
+      'zkapps/roadmap',
+      'zkapps/faq',
+      'zkapps/zkapps-for-ethereum-developers',
       ],
     },
     {
