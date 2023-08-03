991001202b1013cd3027561248d63c0bf3eb93c3
ymekuria <ymekuria@gmail.com>
2023-08-03 12:59:15 -0700

    feat(ghp-postbuild.js): add a postbuild script to prepend the github repo to asset urls for github deployment

---
+++ examples/zkapps/04-zkapp-browser-ui/ui/ghp-postbuild.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+// This script modifies the built CSS files and prepends the repo-name to the asset URLs.
+// to be compatible with github pages deployment.
+const cssDir = path.join(__dirname, '/.next/static/css');
+
+fs.readdir(cssDir, (err, files) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  files.forEach(file => {
+    if (path.extname(file) === '.css') {
+      const filePath = path.join(cssDir, file);
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        }
+        
+        // 04-zkapp-browser-ui should be replaced with your repo name if it is different.
+        const result = data.replace(/url\(\//g, `url(/04-zkapp-browser-ui/`);
+
+        fs.writeFile(filePath, result, 'utf8', err => {
+          if (err) {
+            console.error(err);
+            process.exit(1);
+          }
+        });
+      });
+    }
+  });
+});
