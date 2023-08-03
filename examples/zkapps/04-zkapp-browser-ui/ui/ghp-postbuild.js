2263d5a056c540ee4da07c4c4a4c1bc41e7864f5
ymekuria <ymekuria@gmail.com>
2023-08-03 14:03:50 -0700

    feat(ghp-postbuild.js): add comment to guide user to add their repo name

--- examples/zkapps/04-zkapp-browser-ui/ui/ghp-postbuild.js
+++ examples/zkapps/04-zkapp-browser-ui/ui/ghp-postbuild.js
@@ -4,6 +4,7 @@ const path = require('path');
 // This script modifies the built CSS files and prepends the repo-name to the asset URLs.
 // to be compatible with github pages deployment.
 const cssDir = path.join(__dirname, '/.next/static/css');
+// Add your repository name here.
 let repoURL = '';
 fs.readdir(cssDir, (err, files) => {
   if (err) {
@@ -20,7 +21,7 @@ fs.readdir(cssDir, (err, files) => {
           process.exit(1);
         }
         
-        // 04-zkapp-browser-ui should be replaced with your repo name if it is different.
+
         const result = data.replace(/url\(\//g, `url(/${repoURL}/`);
 
         fs.writeFile(filePath, result, 'utf8', err => {
