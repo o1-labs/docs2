ab682c6ed7a214b4c54ce551f9417f81633c5f5e
Martin Minkov <minkovlmartin@gmail.com>
2023-09-04 15:10:17 -0700

    fix(tutorial4): remove nextjs output from git

--- examples/zkapps/04-zkapp-browser-ui/ui/out/_next/static/IbSPwN67V1Yvix1r0yGn1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/cc8f0cfa-d8c4f2716060e29e.js","static/css/89c9e47031561401.css","static/chunks/pages/index-2b9d223f125d01f0.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
