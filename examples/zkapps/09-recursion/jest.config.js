4ce515cb071747b4dc848283504f06bc1fdc0697
barriebyron <barrie@o1labs.org>
2023-10-06 13:07:20 -0400

    move code from private repo to docs2 repo zkapp examples

---
+++ examples/zkapps/09-recursion/jest.config.js
@@ -0,0 +1,23 @@
+/** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
+export default {
+  verbose: true,
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  transform: {
+    '^.+\\.(t)s$': 'ts-jest',
+    '^.+\\.(j)s$': 'babel-jest',
+  },
+  resolver: '<rootDir>/jest-resolver.cjs',
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!snarkyjs/node_modules/tslib)',
+  ],
+  modulePathIgnorePatterns: ['<rootDir>/build/'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.+)\\.js$': '$1',
+  }
+};
---
+++ examples/zkapps/09-recursion/src/main.ts
@@ -0,0 +1,92 @@
+import { Square } from './Square.js';
+import {
+  isReady,
+  shutdown,
+  Field,
+  Mina,
+  PrivateKey,
+  AccountUpdate,
+  SelfProof,
+  Experimental,
+  Struct,
+  Bool,
+  Circuit,
+  Poseidon,
+  MerkleMap,
+  MerkleTree,
+  MerkleWitness,
+  MerkleMapWitness,
+  verify,
+} from 'snarkyjs';
+
+class MerkleWitness20 extends MerkleWitness(20) {}
+
+async function main() {
+  await isReady;
+
+  console.log('SnarkyJS loaded');
+
+  console.log('compiling...');
+
+  const { verificationKey } = await Add.compile();
+
+  console.log('making proof 0')
+
+  const proof0 = await Add.init(Field(0));
+
+  console.log('making proof 1')
+
+  const proof1 = await Add.addNumber(Field(4), proof0, Field(4));
+
+  console.log('making proof 2')
+
+  const proof2 = await Add.add(Field(4), proof1, proof0);
+
+  console.log('verifying proof 2');
+  console.log('proof 2 data', proof2.publicInput.toString());
+
+  const ok = await verify(proof2.toJSON(), verificationKey);
+  console.log('ok', ok);
+
+  console.log('Shutting down');
+
+  await shutdown();
+}
+
+const Add = Experimental.ZkProgram({
+  publicInput: Field,
+
+  methods: {
+    init: {
+      privateInputs: [],
+
+      method(state: Field) {
+        state.assertEquals(Field(0));
+      },
+    },
+
+    addNumber: {
+      privateInputs: [SelfProof, Field ],
+
+      method(newState: Field, earlierProof: SelfProof<Field>, numberToAdd: Field) {
+        earlierProof.verify();
+        newState.assertEquals(earlierProof.publicInput.add(numberToAdd));
+      },
+    },
+
+    add: {
+      privateInputs: [ SelfProof, SelfProof ],
+
+      method(
+        newState: Field, 
+        earlierProof1: SelfProof<Field>,
+        earlierProof2: SelfProof<Field>,
+      ) {
+        earlierProof1.verify();
+        earlierProof2.verify();
+        newState.assertEquals(earlierProof1.publicInput.add(earlierProof2.publicInput));
+      },
+    },
+  },
+});
+main();
