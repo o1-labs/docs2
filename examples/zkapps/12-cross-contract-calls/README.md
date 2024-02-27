3a1ce8552ec6b5905169b17f4992192381341082
Saurabh Patil <55076843+Saurabhpatil-dev@users.noreply.github.com>
2024-02-27 17:28:19 +0530

    :memo: Updated Readme & audited contract, all working

--- examples/zkapps/12-cross-contract-calls/README.md
+++ examples/zkapps/12-cross-contract-calls/README.md
@@ -1,25 +1,49 @@
 # Mina zkApp: 12 Cross Contract Calls
 
-This template uses TypeScript.
+This Cross Contract Calls tutorial helps you learn how smart contracts on a blockchain can interact by calling functions in each other's code, enabling building modular and complex decentralized applications.
 
-## How to build
+## Version
+- o1js: **0.16.2**
 
-```sh
-npm run build
-```
+## Tutorial
 
-## How to run tests
+For the step-by-step tutorial, see [Tutorial 12: Cross Contract Calls](https://docs.minaprotocol.com/zkapps/tutorials/cross-contract-calls).
 
-```sh
-npm run test
-npm run testw # watch mode
-```
+## How to install and run this example project
 
-## How to run coverage
+1. Clone the repository:
+    ```sh
+    git clone https://github.com/o1-labs/docs2.git
+    ```
+2. Change directory to the project location:
+    ```sh
+    cd docs2/examples/zkapps/12-cross-contract-calls
+    ```
+3. Install dependencies:
+    ```sh
+    npm install
+    ```
 
-```sh
-npm run coverage
-```
+4. Build the project:
+    ```sh
+    npm run build
+    ```
+
+5. Run the compiled code:
+    ```sh
+    node build/src/composability.js
+    ```
+    To run and build the compiled code with a single command:
+    ```sh
+    npm run build && node build/src/composability.js
+    ```
+
+## Expected output
+![12output](12output.png)
+
+## Last audit date
+ 
+**27-February-2024**
 
 ## License
 
--- examples/zkapps/12-cross-contract-calls/src/composability.ts
+++ examples/zkapps/12-cross-contract-calls/src/composability.ts
@@ -9,15 +9,12 @@ import {
   State,
 } from 'o1js';
 
-// contract which can add 1 to a number
 class Incrementer extends SmartContract {
   @method increment(x: Field): Field {
     return x.add(1);
   }
 }
 
-// contract which can add two numbers, plus 1, and return the result
-// incrementing by one is outsourced to another contract (it's cleaner that way, we want to stick to the single responsibility principle)
 class Adder extends SmartContract {
   @method addPlus1(x: Field, y: Field): Field {
     // compute result
@@ -28,7 +25,6 @@ class Adder extends SmartContract {
   }
 }
 
-// contract which calls the Adder, stores the result on chain & emits an event
 class Caller extends SmartContract {
   @state(Field) sum = State<Field>();
   events = { sum: Field };
@@ -46,17 +42,15 @@ const doProofs = true;
 let Local = Mina.LocalBlockchain({ proofsEnabled: doProofs });
 Mina.setActiveInstance(Local);
 
-// a test account that pays all the fees, and puts additional funds into the zkapp
 let feePayerKey = Local.testAccounts[0].privateKey;
 let feePayer = Local.testAccounts[0].publicKey;
 
-// the first contract's address
 let incrementerKey = PrivateKey.random();
 let incrementerAddress = incrementerKey.toPublicKey();
-// the second contract's address
+
 let adderKey = PrivateKey.random();
 let adderAddress = adderKey.toPublicKey();
-// the third contract's address
+
 let zkappKey = PrivateKey.random();
 let zkappAddress = zkappKey.toPublicKey();
 
@@ -75,7 +69,6 @@ if (doProofs) {
 
 console.log('deploy');
 let tx = await Mina.transaction(feePayer, () => {
-  // TODO: enable funding multiple accounts properly
   AccountUpdate.fundNewAccount(feePayer, 3);
   zkapp.deploy();
   adderZkapp.deploy();
@@ -85,7 +78,6 @@ await tx.sign([feePayerKey, zkappKey, adderKey, incrementerKey]).send();
 
 console.log('call interaction');
 tx = await Mina.transaction(feePayer, () => {
-  // we just call one contract here, nothing special to do
   zkapp.callAddAndEmit(Field(5), Field(6));
 });
 console.log('proving (3 proofs.. can take a bit!)');
@@ -93,5 +85,4 @@ await tx.prove();
 console.log(tx.toPretty());
 await tx.sign([feePayerKey]).send();
 
-// should hopefully be 12 since we added 5 + 6 + 1
 console.log('state: ' + zkapp.sum.get());
