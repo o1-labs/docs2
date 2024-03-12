2871400ff43e06d021fc1b424002907b0102f545
ymekuria <ymekuria@gmail.com>
2024-03-11 19:07:03 -0600

    feat(Square.ts): make the update @method async

--- examples/zkapps/01-hello-world/src/Square.ts
+++ examples/zkapps/01-hello-world/src/Square.ts
@@ -1,10 +1,4 @@
-import { 
-  Field, 
-  SmartContract, 
-  state, 
-  State, 
-  method 
-} from 'o1js';
+import { Field, SmartContract, state, State, method } from 'o1js';
 
 export class Square extends SmartContract {
   @state(Field) num = State<Field>();
@@ -14,7 +8,7 @@ export class Square extends SmartContract {
     this.num.set(Field(3));
   }
 
-  @method update(square: Field) {
+  @method async update(square: Field) {
     const currentState = this.num.get();
     this.num.requireEquals(currentState);
     square.assertEquals(currentState.mul(currentState));
