72b995ae74cf166847781b5bc278f37faf90b53b
ymekuria <ymekuria@gmail.com>
2024-03-12 18:58:50 -0700

    feat(IncrementSecret.ts):  add async to initState @method

--- examples/zkapps/02-private-inputs-and-hash-functions/src/IncrementSecret.ts
+++ examples/zkapps/02-private-inputs-and-hash-functions/src/IncrementSecret.ts
@@ -3,7 +3,7 @@ import { Field, SmartContract, state, State, method, Poseidon } from 'o1js';
 export class IncrementSecret extends SmartContract {
   @state(Field) x = State<Field>();
 
-  @method initState(salt: Field, firstSecret: Field) {
+  @method async initState(salt: Field, firstSecret: Field) {
     this.x.set(Poseidon.hash([salt, firstSecret]));
   }
 
