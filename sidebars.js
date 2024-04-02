491fcc433eb67dacba6a59daf82d01edfb1c9b88
barriebyron <barrie@o1labs.org>
2024-04-02 09:02:48 -0400

    apply self-describing links for re-organized migration docs

--- sidebars.js
+++ sidebars.js
@@ -575,6 +575,7 @@ module.exports = {
         'berkeley-upgrade/archive-migration-prerequisites',
         'berkeley-upgrade/archive-migration-installation',
         'berkeley-upgrade/migrating-archive-database-to-berkeley',
+        `berkeley-upgrade/mainnet-database-maintenance`,
         'berkeley-upgrade/worked-archive-example',
       ],
     },
