ee34718c6e2676f9c66b9137248b8967c05db5f2
ymekuria <ymekuria@gmail.com>
2024-04-11 09:54:57 -0700

    refactor: removed unused example

--- examples/zkapps/08-custom-tokens/src/BasicTokenContract.test.ts
+++
@@ -1,7 +0,0 @@
-import { BasicTokenContract } from './BasicTokenContract.js';
-
-describe('BasicTokenContract.ts.js', () => {
-  describe('BasicTokenContract.ts()', () => {
-    it.todo('should be correct');
-  });
-});
